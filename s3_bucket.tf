cc8b11f9e2d6b7c69a7b9aa2696db1296be97337
pranit-p <pranit.pawar@medly.com>
2022-02-02 21:33:37 +0530

    Add secrete block

--- s3_bucket.tf
+++ s3_bucket.tf
@@ -7,3 +7,11 @@ resource "aws_s3_bucket" "b" {
     Environment = "Dev"
   }
 }
+
+resource "aws_secretsmanager_secret_version" "for_testing_integration_url" {
+  secret_id     = aws_secretsmanager_secret.pagerduty_integration_url.id
+  secret_string = "invalid_value"
+  lifecycle {
+    ignore_changes = [secret_string]
+  }
+}
