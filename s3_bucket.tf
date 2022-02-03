2ceae143d60582eee36f9fb7da919032745c44d4
pranit-p <pranit.pawar@medly.com>
2022-02-03 11:02:23 +0530

    Add commentor

--- s3_bucket.tf
+++ s3_bucket.tf
@@ -1,17 +1,17 @@
-resource "aws_s3_bucket" "b" {
-  bucket = "testing0-s3-bucket"
-  acl    = "private"
+# resource "aws_s3_bucket" "b" {
+#   bucket = "testing0-s3-bucket"
+#   acl    = "private"
 
-  tags = {
-    Name        = "My bucket"
-    Environment = "Dev"
-  }
-}
+#   tags = {
+#     Name        = "My bucket"
+#     Environment = "Dev"
+#   }
+# }
 
-resource "aws_secretsmanager_secret_version" "for_testing_integration_url" {
-  secret_id     = aws_secretsmanager_secret.pagerduty_integration_url.id
-  secret_string = "invalid_value"
-  lifecycle {
-    ignore_changes = [secret_string]
-  }
-}
+# resource "aws_secretsmanager_secret_version" "for_testing_integration_url" {
+#   secret_id     = aws_secretsmanager_secret.pagerduty_integration_url.id
+#   secret_string = "invalid_value"
+#   lifecycle {
+#     ignore_changes = [secret_string]
+#   }
+# }
